7c425804883118593deab6b320f3f4ecda852dbd
SkJaan110 <106261813+SkJaan110@users.noreply.github.com>
2025-09-30 10:27:10 +0500

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+
+// Tumhara Firebase config (jo Firebase console se mila hai)
+const firebaseConfig = {
+  apiKey: "AIzaSyC1dlbXXE5bUgt_KrUa5ALqtWx61qTipuU",
+  authDomain: "sk-scoring-cric.firebaseapp.com",
+  databaseURL: "https://sk-scoring-cric-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sk-scoring-cric",
+  storageBucket: "sk-scoring-cric.appspot.com",
+  messagingSenderId: "481008550157",
+  appId: "1:481008550157:web:483f49ff838812946f044",
+  measurementId: "G-S4MSGJMLBB"
+};
+
+// Firebase initialize
+firebase.initializeApp(firebaseConfig);
