8dcc1d40be0f2f7b1ceaed59c8e6d356869c41b1
SkJaan110 <106261813+SkJaan110@users.noreply.github.com>
2025-10-01 13:27:32 +0500

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 // firebase-config.js
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyC1dlbXXE5bUgt_KrUa5ALqtWx61qTipU8",
   authDomain: "sk-scoring-cric.firebaseapp.com",
