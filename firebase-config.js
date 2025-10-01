cdaf8e1cdc52ceeec98e0540c82c11a3127b008e
SkJaan110 <106261813+SkJaan110@users.noreply.github.com>
2025-10-01 08:56:42 +0500

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyC1dbXXE5bUgt_KrUa5ALqtWx61qTipuU8",
+  apiKey: "AIzaSyC1dlbXXE5bUgt_KrUa5ALqtWx61qTipU8",
   authDomain: "sk-scoring-cric.firebaseapp.com",
   databaseURL: "https://sk-scoring-cric-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "sk-scoring-cric",
   storageBucket: "sk-scoring-cric.appspot.com",
   messagingSenderId: "481008550157",
   appId: "1:481008550157:web:483f49ff383812946f044",
-  measurementId: "G-S4MSGJMLBB"
+  measurementId: "G-S4MSGJBMLB"
 };
 
 // Initialize Firebase
