11544e75cf171f888f4a2b6e24a3edb87a793bd6
SkJaan110 <106261813+SkJaan110@users.noreply.github.com>
2025-10-01 13:21:51 +0500

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+// firebase-config.js
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC1dlbXXE5bUgt_KrUa5ALqtWx61qTipU8",
@@ -9,3 +10,9 @@ const firebaseConfig = {
   appId: "1:481008550157:web:483f4f9ff383812946f044",
   measurementId: "G-S4MSGJBMLB"
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Database reference helper
+const db = firebase.database();
